aa679498830e40119d5abf0e63252c72ad9fef02
Pablo De Nápoli <pdenapo@gmail.com>
2016-09-26 16:31:42 -0300

    Add a python script to checkout the ReactOS sources.

---
+++ check_out_react_os.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# Script to checkout the React OS sources
+# Copyright (C) 2016  Pablo De Nápoli <pdenapo@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Dependencies: svn
+
+from __future__ import print_function
+import subprocess
+import argparse
+
+def checkout_reactos_trunk(revision):
+    """ This function runs the "svn info" command on the React OS subversion repository, with outpoot in xml format in the file svn_info.xml in the current directory."""
+
+    global config
+    print('Running svn checkout for getting the current React OS version')
+    subprocess.call(['svn', 'checkout','--revision',revision, 'svn://svn.reactos.org/reactos/trunk/reactos',config["reactos_src_dir"]])
+    
+
+if __name__ == '__main__':
+
+    # We parse the command-line arguments
+
+    parser = \
+        argparse.ArgumentParser(description='Create a VirtualBox virtual machine for a given revision of React-OS.'
+                                )
+    parser.add_argument('--revision', dest='revision',
+                        help='for which revision? [default=current revision]',
+                        default='HEAD'
+                        )
+    parser.add_argument('--config', dest='config_file_name',
+                        help='configuration file to read', default='config.json'
+                        )
+
+    args = parser.parse_args()
+    
+    # We use a configuration file with json format for several configuration parameters.
+
+    if os.path.isfile(args.config_file_name):
+        config = json.load(open(args.config_file_name))
+    else:
+        print('Configuration file',args.config_file_name,' does not exist.')
+        sys.exit(3)
+  
+    revision = args.revision
+     
+    checkout_reactos_trunk(revision)
