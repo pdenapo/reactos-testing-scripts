c2a462b9c347501e6492e538ce2b0c91cfad6d86
Pablo De Nápoli <pdenapo@gmail.com>
2016-10-02 12:19:43 -0300

    Adding a bash script for compiling React OS (Pure python didn't work!)

---
+++ compile_reactos.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+# Bash script for compiling ReactOs - Should be run under ReactOS BE
+# Dependencies: uses jq for parsing the config.json file
+CONFIG_FILE="config.json"
+SRC_DIR=$(jq --raw-output .reactos_src_dir < $CONFIG_FILE)
+if [[ $SRC_DIR ]]; then
+    echo "reactos_src_dir= ${SRC_DIR}"
+else
+    echo "reactos_src_dir not defined in $CONFIG_FILE"
+    exit 1
+fi
+BUILD_DIR=$(jq --raw-output .reactos_build_dir < $CONFIG_FILE)
+if [[ $BUILD_DIR ]]; then
+    echo "reactos_build_dir= ${SRC_DIR}"
+else
+    echo "reactos_build_dir not defined in $CONFIG_FILE"
+    exit 1
+fi
+rm --recursive --force rosbuild
+mkdir rosbuild
+cd $BUILD_DIR
+$SRC_DIR/reactos/configure.sh
+cd $BUILD_DIR/reactos
+ninja bootcd
+
